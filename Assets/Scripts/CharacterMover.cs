f16ca50bff61e2145ba6914fd4d292fa5295a52f
JadenCooper <jaden_cooper@outlook.com>
2023-02-09 00:34:50 +1300

    Added Animation And Charcter Flip

--- Assets/Scripts/CharacterMover.cs
+++ Assets/Scripts/CharacterMover.cs
@@ -8,15 +8,18 @@ public class CharacterMover : MonoBehaviour
 {
     private Vector2 movementVector;
     private Vector2 movement;
+    private Animator animator;
     public Rigidbody2D rb2d;
     public float Speed;
     public float currentSpeed;
     public float Acceleration;
     public float MaxSpeed;
     public float currentForewardDirection;
+    private bool isMoving;
     private void Awake()
     {
         rb2d = GetComponentInParent<Rigidbody2D>();
+        animator = GetComponentInParent<Animator>();
     }
 
     public void Move(Vector2 movementVector)
@@ -33,16 +36,34 @@ public class CharacterMover : MonoBehaviour
         if (MathF.Abs(movementVector.y) == 0 && MathF.Abs(movementVector.x) == 0)
         {
             currentSpeed += -Acceleration * Time.deltaTime;
+            isMoving = false;
         }
         else
         {
             currentSpeed += Acceleration * Time.deltaTime;
+            isMoving = true;
+            CheckSide();
         }
         currentSpeed = Mathf.Clamp(currentSpeed, 0, MaxSpeed);
+    }
+
+    private void CheckSide()
+    {
+        if (movementVector.x < 0)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+            //gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            //gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
     private void FixedUpdate()
     {
         rb2d.velocity = movement * Time.deltaTime;
+        animator.SetBool("isMoving", isMoving);
         //Debug.Log("R" + rb2d.velocity);
     }
 }
